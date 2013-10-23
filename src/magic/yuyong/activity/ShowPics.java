c9a69e7c2fb043a9bc72c398b25e889e7e6e1d00
wanyuyong <wanyuyong@gmail.com>
2013-10-23 17:56:20 +0800

    change twitter show menu

--- src/magic/yuyong/activity/ShowPics.java
+++ src/magic/yuyong/activity/ShowPics.java
@@ -81,7 +81,7 @@ public class ShowPics extends BaseActivity {
 		setContentView(R.layout.show_pics);
 		mJazzy = (JazzyViewPager) findViewById(R.id.jazzy_pager);
 		mJazzy.setTransitionEffect(TransitionEffect.Tablet);
-		mJazzy.setOffscreenPageLimit(1);
+		mJazzy.setOffscreenPageLimit(2);
 		adapter = new ShowPicAdapter();
 		adapter.setJazzy(mJazzy);
 
